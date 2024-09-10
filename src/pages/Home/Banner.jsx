2bf6ed002844fcadfaeb792481ac1490c373fd14
jaheda8256 <jahedasultana47@gmail.com>
2024-09-10 19:18:59 +0600

    added hover:color

--- src/pages/Home/Banner.jsx
+++ src/pages/Home/Banner.jsx
@@ -1,36 +1,4 @@
-// import img3 from '/sathi-sathi.png';
 
-// const Banner = () => {
-//   return (
-//     <section className=" dark:text-gray-800">
-//       <div className="max-w-screen-lg flex flex-col justify-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-evenly">
-//         <div className="">
-//           <img
-//             src={img3}
-//             alt=""
-//             className="object-cover rounded-full size-72 border-4 p-2 border-[#8d918b] md:size-96"
-//           />
-//         </div>
-//         <div className="flex flex-col justify-center p-6 text-center rounded-sm lg:max-w-md xl:max-w-lg space-y-3">
-//           <h3 className="text-xl font-semibold text-[#8d918b]">Hello, I'm</h3>
-//           <h1 className="text-5xl font-bold">Jaheda Sultana</h1>
-//           <h1 className="text-3xl text-[#8d918b] font-bold">
-//             Frontend Developer
-//           </h1>
-
-//           <div className="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4">
-//             <a target="_blank" href="https://drive.google.com/file/d/1tfkmsdxVIITUD03oakAyv4Y21NrD-LED/view?usp=sharing" className="btn btn-outline border-[#596475] font-medium rounded-full hover:bg-[#596475]">Download CV</a>
-//             <a href="#contact" className="btn text-white font-medium hover:bg-[#140f2d] bg-[#596475] rounded-full py-2">
-//               Contact Info
-//             </a>
-//           </div>
-//         </div>
-//       </div>
-//     </section>
-//   );
-// };
-
-// export default Banner;
 
 import { FaFacebook, FaGithub } from "react-icons/fa";
 import img3 from "../../assets/sumu.png";
@@ -54,9 +22,6 @@ const Banner = () => {
           <div className="text-4xl text-gray-600 md:text-xl mb-4">
             Hi There,
           </div>
-          {/* <h1 className="text-3xl md:text-4xl font-extrabold mb-4">
-            I Am a Frontend Developer
-          </h1> */}
 
           <div>
           <h2 className="text-3xl text-black font-extrabold">I Am a  <span style={{ color: 'black', fontWeight: 'extrabold', fontSize: 'text-3xl' }}>
@@ -86,7 +51,7 @@ const Banner = () => {
             </a>
             <a
               href="#contact"
-              className="btn text-white font-medium hover:bg-[#41a909] bg-[#7ac74f] rounded-full py-2"
+              className="btn text-white font-medium hover:bg-[#41a909] hover:underline bg-[#7ac74f] rounded-full py-2"
             >
               Contact Info
             </a>
--- src/pages/Home/Projects.jsx
+++ src/pages/Home/Projects.jsx
@@ -9,16 +9,16 @@ const Projects = () => {
       <div className="grid md:grid-cols-3 gap-4">
     
         <div className="border-2 shadow-xl rounded-md w-full bg-white">
-          <figure>
+          <figure className='overflow-hidden'>
             <img
               src={img1}
-              className="w-full h-[350px] object-cover object-top rounded-t-md"
+              className="w-full hover:scale-150 transition-all duration-700 h-[350px] object-cover object-top rounded-t-md"
               alt=""
             />
           </figure>
           <div className="p-4">
             <h3 className="text-2xl font-bold">Fitness Tracker</h3>
-            <p className="text-[#8d918b] mt-1 mb-1">FitnessTracker is a comprehensive fitness management platform designed to empower individuals to achieve their health.</p>
+           
             <p className="text-[#8d918b] mt-1 mb-1">
               <span className="font-bold">Technologies:</span> react-router-dom, JWT for authentication, React-simple-typewriter
             </p>
@@ -28,14 +28,14 @@ const Projects = () => {
               <a
                 href='https://jade-gingersnap-ed17f2.netlify.app'
                 target="_blank"
-                className="inline-block py-2 px-3 md:px-4 text-sm md:text-base bg-slate-600 text-white rounded-md hover:underline hover:-translate-y-0.5 hover:cursor-pointer"
+                className="inline-block py-2 px-3 md:px-4 text-sm md:text-base hover:bg-[#41a909] bg-[#7ac74f] text-white rounded-md hover:underline hover:-translate-y-0.5 hover:cursor-pointer"
               >
                 Live site
               </a>
               <a
                 href='https://github.com/jahedasultana/pha12-fitness-tracker-client'
                 target="_blank"
-                className="inline-block py-2 px-3 md:px-4 text-sm md:text-base bg-slate-600 text-white rounded-md hover:underline hover:-translate-y-0.5 hover:cursor-pointer"
+                className="inline-block py-2 px-3 md:px-4 text-sm md:text-base hover:bg-[#41a909] bg-[#7ac74f] text-white rounded-md hover:underline hover:-translate-y-0.5 hover:cursor-pointer"
               >
                 Source code
               </a>
@@ -43,7 +43,7 @@ const Projects = () => {
                 <a
                   href='https://github.com/jahedasultana/pha12-fitness-tracker-server'
                   target="_blank"
-                  className="inline-block py-2 px-3 md:px-4 text-sm md:text-base bg-slate-600 text-white rounded-md hover:underline hover:-translate-y-0.5 hover:cursor-pointer"
+                  className="inline-block py-2 px-3 md:px-4 text-sm md:text-base hover:bg-[#41a909] bg-[#7ac74f] text-white rounded-md hover:underline hover:-translate-y-0.5 hover:cursor-pointer"
                 >
                   Server code
                 </a>
@@ -52,16 +52,15 @@ const Projects = () => {
           </div>
         </div>
         <div className="border-2 shadow-xl rounded-md w-full bg-white">
-          <figure>
+        <figure className='overflow-hidden'>
             <img
               src={img2}
-              className="w-full h-[350px] object-cover object-top rounded-t-md"
+              className="w-full hover:scale-150 transition-all duration-700 h-[350px] object-cover object-top rounded-t-md"
               alt=""
             />
           </figure>
           <div className="p-4">
             <h3 className="text-2xl font-bold">Food-donate</h3>
-            <p className="text-[#8d918b] mt-1 mb-1">We are developing a Community Food Sharing and Surplus Reduction Platform where users can share surplus food items with others in need</p>
             <p className="text-[#8d918b] mt-1 mb-1">
               <span className="font-bold">Technologies:</span> react-router-dom, JWT for authentication, React-simple-typewriter
             </p>
@@ -71,14 +70,14 @@ const Projects = () => {
               <a
                 href='https://food-projects-9d8e7.web.app'
                 target="_blank"
-                className="inline-block py-2 px-3 md:px-4 text-sm md:text-base bg-slate-600 text-white rounded-md hover:underline hover:-translate-y-0.5 hover:cursor-pointer"
+                className="inline-block py-2 px-3 md:px-4 text-sm md:text-base hover:bg-[#41a909] bg-[#7ac74f] text-white rounded-md hover:underline hover:-translate-y-0.5 hover:cursor-pointer"
               >
                 Live site
               </a>
               <a
                 href='https://github.com/jahedasultana/pha11-food-donator-client'
                 target="_blank"
-                className="inline-block py-2 px-3 md:px-4 text-sm md:text-base bg-slate-600 text-white rounded-md hover:underline hover:-translate-y-0.5 hover:cursor-pointer"
+                className="inline-block py-2 px-3 md:px-4 text-sm md:text-base hover:bg-[#41a909] bg-[#7ac74f] text-white rounded-md hover:underline hover:-translate-y-0.5 hover:cursor-pointer"
               >
                 Source code
               </a>
@@ -86,7 +85,7 @@ const Projects = () => {
                 <a
                   href='https://github.com/jahedasultana/pha11-food-donator-server'
                   target="_blank"
-                  className="inline-block py-2 px-3 md:px-4 text-sm md:text-base bg-slate-600 text-white rounded-md hover:underline hover:-translate-y-0.5 hover:cursor-pointer"
+                  className="inline-block py-2 px-3 md:px-4 text-sm md:text-base hover:bg-[#41a909] bg-[#7ac74f] text-white rounded-md hover:underline hover:-translate-y-0.5 hover:cursor-pointer"
                 >
                   Server code
                 </a>
@@ -95,16 +94,16 @@ const Projects = () => {
           </div>
         </div>
         <div className="border-2 shadow-xl rounded-md w-full bg-white">
-          <figure>
+        <figure className='overflow-hidden'>
             <img
               src={img3}
-              className="w-full h-[350px] object-cover object-top rounded-t-md"
+              className="w-full hover:scale-150 transition-all duration-700 h-[350px] object-cover object-top rounded-t-md"
               alt=""
             />
           </figure>
           <div className="p-4">
             <h3 className="text-2xl font-bold">Tourists</h3>
-            <p className="text-[#8d918b] mt-1 mb-1">Explore Southeast Asia is a tourism management website focusing on the diverse and vibrant tourist destinations within Southeast Asia.</p>
+ 
             <p className="text-[#8d918b] mt-1 mb-1">
               <span className="font-bold">Technologies:</span> react-router-dom, JWT for authentication, React-simple-typewriter
             </p>
@@ -114,14 +113,14 @@ const Projects = () => {
               <a
                 href='https://tourist-website-e895b.web.app'
                 target="_blank"
-                className="inline-block py-2 px-3 md:px-4 text-sm md:text-base bg-slate-600 text-white rounded-md hover:underline hover:-translate-y-0.5 hover:cursor-pointer"
+                className="inline-block py-2 px-3 md:px-4 text-sm md:text-base hover:bg-[#41a909] bg-[#7ac74f] text-white rounded-md hover:underline hover:-translate-y-0.5 hover:cursor-pointer"
               >
                 Live site
               </a>
               <a
                 href='https://github.com/jahedasultana/pha11-food-donator-client'
                 target="_blank"
-                className="inline-block py-2 px-3 md:px-4 text-sm md:text-base bg-slate-600 text-white rounded-md hover:underline hover:-translate-y-0.5 hover:cursor-pointer"
+                className="inline-block py-2 px-3 md:px-4 text-sm md:text-base hover:bg-[#41a909] bg-[#7ac74f] text-white rounded-md hover:underline hover:-translate-y-0.5 hover:cursor-pointer"
               >
                 Source code
               </a>
@@ -129,7 +128,7 @@ const Projects = () => {
                 <a
                   href='https://github.com/jahedasultana/pha10-tourist-server'
                   target="_blank"
-                  className="inline-block py-2 px-3 md:px-4 text-sm md:text-base bg-slate-600 text-white rounded-md hover:underline hover:-translate-y-0.5 hover:cursor-pointer"
+                  className="inline-block py-2 px-3 md:px-4 text-sm md:text-base hover:bg-[#41a909] bg-[#7ac74f] text-white rounded-md hover:underline hover:-translate-y-0.5 hover:cursor-pointer"
                 >
                   Server code
                 </a>
